a331497b6cb1349201f0c4b676da95531770baa7
Barnabas Nduwak Suku <barnssuku@gmail.com>
2018-12-24 13:45:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var toPlay = [];
+  for (var counter = 0; counter < musicians.length; musicians++) {
+    var toPush = musicians[i] + " plays "+ instruments[i];
+    toPlay.push(toPush);
+  }
+  return toPlay;
+}
+
+function johnLennonFacts(johnLennonFacts) {
+  var new_array = [];
+  var counter = 0;
+  while (counter < johnLennonFacts.length) {
+    new_array.push(johnLennonFacts[counter] + "!!!")
+    counter++;
+  }
+  return new_array;
+}
+
+function iLoveTheBeatles(counter) {
+  do {
+    counter++;
+    new_array.push("I love the Beatles!");    
+  } while (counter < 15);
+}
